d46d2ba10336a24fdc3220041d3dd0d798af20e4
VexV0t <92925112+vexb0t@users.noreply.github.com>
2023-10-28 06:40:09 -0700

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import netlify from '@astrojs/netlify/functions';
+// https://astro.build/config
+export default defineConfig({
+	output: 'server',
+	integrations: [svelte()],
+	adapter: netlify(),
+});
